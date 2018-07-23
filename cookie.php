031b4b84ab6aaba06668e45ff4bfc61f4edd484a
zhuhui901222 <15812902593@139.com>
2018-07-23 17:11:45 +0800

    first commit

---
+++ cookie.php
@@ -0,0 +1,10 @@
+<?php
+
+header('P3P: CP="CURa ADMa DEVa PSAo PSDo OUR BUS UNI PUR INT DEM STA PRE COM NAV OTC NOI DSP COR"');   
+setcookie('keyy','ghhhhhhhhhhhh'.rand(1,999),time()+3600,'/','9201314.club');
+
+$_SESSION["username"]="skygao222";
+
+
+echo 123;
+echo $_SESSION["username"];
---
+++ curl.php
@@ -0,0 +1,9 @@
+<?php
+
+echo '请求设置cookie';
+
+echo '<script src="http://www.9201314.club/cookie.php"></script>';
+
+?>
+
+
